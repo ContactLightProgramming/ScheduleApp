9d01dbf78004f520bc1ed3b142ee6ee2c5232e80
jlindvall <joa.lindvall@gmail.com>
2017-10-05 16:33:47 +0200

    test

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -23,6 +23,28 @@ public class Employee {
         this.pnr = pnr;
     }
 
+    @javax.persistence.JoinColumn(name = "fname")
+    private String fname;
+
+    public String getFname(){
+        return fname;
+    }
+
+    public void setFname(String lname){
+        this.fname = fname;
+    }
+
+    @javax.persistence.JoinColumn(name = "lname")
+    private String lname;
+
+    public String getLname(){
+        return lname;
+    }
+
+    public void setLname(String lname){
+        this.lname = lname;
+    }
+
 
 
 
